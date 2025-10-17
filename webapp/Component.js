b3fd2a27bf5071a4d29fc6e05ba454492c6e866e
Mayko Facci <maykofacci@Mayko-M3-Air.local>
2025-10-17 14:45:21 -0300

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+],
+    function (UIComponent) {
+        "use strict";
+
+        return UIComponent.extend("sap.training.exc.Component", {
+            
+            metadata: {
+                manifest: "json"
+            },
+
+            init: function () {
+                //call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+            }
+
+        });
+    }
+);
