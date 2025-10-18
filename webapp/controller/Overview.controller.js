816f8a4bb148ec1781610b77069eaf8cbe340272
Mayko Facci <maykofacci@Mayko-M3-Air.local>
2025-10-18 12:08:40 -0300

    Implementing element binding

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -34,6 +34,11 @@ sap.ui.define([
 
             onCloseDialog: function () {
                 this.byId("dialog").close();
+            },
+
+            onCustomerChange: function (oEvent) {
+                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+                this.byId("bookingTable").setBindingContext(oBindingContext);
             }
         });
     });
